ed62ddd843d237d47e0b9c190a80d8e2b7c6b2fd
yinghanhung <hle.infdy@gmail.com>
2025-10-02 00:49:48 +0800

    introduce the proper way to cancel async call

--- grpc-http1-proxy/src/main/kotlin/org/hle/grpchttp1proxy/client/impl/HelloWorldClientAsyncImpl.kt
+++ grpc-http1-proxy/src/main/kotlin/org/hle/grpchttp1proxy/client/impl/HelloWorldClientAsyncImpl.kt
@@ -4,6 +4,8 @@ import io.grpc.ManagedChannel
 import io.grpc.examples.helloworld.GreeterGrpc
 import io.grpc.examples.helloworld.HelloReply
 import io.grpc.examples.helloworld.HelloRequest
+import io.grpc.stub.ClientCallStreamObserver
+import io.grpc.stub.ClientResponseObserver
 import io.grpc.stub.StreamObserver
 import kotlinx.coroutines.suspendCancellableCoroutine
 import org.hle.grpchttp1proxy.client.HelloWorldClient
@@ -24,7 +26,15 @@ class HelloWorldClientAsyncImpl(channel: ManagedChannel) : HelloWorldClient {
 
         // Make the gRPC call in a non-blocking way using the async stub
         return suspendCancellableCoroutine { continuation ->
-            asyncStub.sayHello(request, object : StreamObserver<HelloReply> {
+            // Only ClientCallStreamObserver has the cancel method.
+            var callObserver: ClientCallStreamObserver<HelloRequest?>? = null
+
+            // For unary call, ClientCallStreamObserver needs to be provided by ClientResponseObserver.
+            val responseObserver = object : ClientResponseObserver<HelloRequest, HelloReply> {
+                override fun beforeStart(p0: ClientCallStreamObserver<HelloRequest?>?) {
+                    callObserver = p0
+                }
+
                 override fun onNext(response: HelloReply) {
                     // Convert from gRPC response to DTO and resume the coroutine
                     continuation.resumeWith(Result.success(response))
@@ -38,13 +48,13 @@ class HelloWorldClientAsyncImpl(channel: ManagedChannel) : HelloWorldClient {
                 override fun onCompleted() {
                     // This is called after onNext for unary calls, so we don't need to do anything here
                 }
-            })
+            }
+            asyncStub.sayHello(request, responseObserver)
 
             // Register cancellation handler
             continuation.invokeOnCancellation {
                 // Cancel the gRPC call if the coroutine is cancelled
-                // Note: gRPC doesn't provide a direct way to cancel individual calls
-                // This is a best-effort approach
+                callObserver?.cancel("Coroutine was cancelled", it)
             }
         }
     }
